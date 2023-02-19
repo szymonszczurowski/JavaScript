327bc159efcdd8599d8f98c988eb1559d7b6891a
Szymon Szczurowski <szczurowski.szymon@wp.pl>
2023-02-19 17:24:49 +0100

    basics

---
+++ js/basics.js
@@ -0,0 +1,20 @@
+console.log('cześć') // wylogowanie słowa w konsoli
+console.log('cześć2') 
+console.log('cześć3') 
+console.log('cześć4') 
+
+//Można użuwać średników, ale nie trzeba
+
+//zmienne
+const name = "Lisa" // zmienna stała (Nie można nadpisywać / Nie można mieć kolejnej zmiennej o takiej samej nazwie)
+console.log(name);
+
+let age = 23 // zmienna lokalna (Można nadpisywać)
+console.log(age);
+age = 40
+console.log(age);
+let colore // można nic nie przypisywać do zmiennej
+
+var test = 123 // var już raczej nie używany, jedynie wspierany w starych projektach
+var test = 1234
+console.log(test);
